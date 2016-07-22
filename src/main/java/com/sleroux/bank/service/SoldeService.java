38e9964f998cd8382afed5e959937be956ef8375
Sylvain Le Roux <sylvain.leroux@gmail.com>
2016-07-22 21:34:02 +0200

    Balances print

--- src/main/java/com/sleroux/bank/service/SoldeService.java
+++ src/main/java/com/sleroux/bank/service/SoldeService.java
@@ -1,5 +1,6 @@
 package com.sleroux.bank.service;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.List;
 
@@ -13,16 +14,19 @@ import com.sleroux.bank.model.AccountBalance;
 public class SoldeService {
 
 	@Autowired
-	IOperationDao	operationDao;
+	IOperationDao operationDao;
 
 	public void run() {
-		
-		DecimalFormat formater = new DecimalFormat("+ ###,###.##;-#");
 
+		DecimalFormat formater = new DecimalFormat("+ ###,###.00;-#");
+		BigDecimal total = new BigDecimal(0);
 		List<AccountBalance> accounts = operationDao.getSoldes();
 		for (AccountBalance c : accounts) {
-			System.out.printf("%-4s: %14s\n", c.getCompte(), formater.format(c.getSolde()));
+			System.out.printf("%-10s: %14s\n", c.getCompte(), formater.format(c.getSolde()));
+			total = total.add(c.getSolde());
 		}
+		System.out.println("--------------------------");
+		System.out.printf("%-10s: %14s\n","Total", formater.format(total));
 
 	}
 
