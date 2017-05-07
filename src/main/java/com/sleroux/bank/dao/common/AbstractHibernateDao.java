b657041362ddc004253cb4fbb1d2e835e602339a
Sylvain Le Roux <sylvain.leroux@gmail.com>
2017-05-07 20:43:39 +0200

    Remove useless dependencies

--- src/main/java/com/sleroux/bank/dao/common/AbstractHibernateDao.java
+++ src/main/java/com/sleroux/bank/dao/common/AbstractHibernateDao.java
@@ -7,59 +7,53 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.google.common.base.Preconditions;
-
 @SuppressWarnings("unchecked")
 public abstract class AbstractHibernateDao<T extends Serializable> implements IOperations<T> {
-    private Class<T> clazz;
+	private Class<T>		clazz;
 
-    @Autowired
-    private SessionFactory sessionFactory;
+	@Autowired
+	private SessionFactory	sessionFactory;
 
-    // API
+	// API
 
-    protected final void setClazz(final Class<T> clazzToSet) {
-        clazz = Preconditions.checkNotNull(clazzToSet);
-    }
+	protected final void setClazz(final Class<T> clazzToSet) {
+		clazz = clazzToSet;
+	}
 
-    @Override
-    public final T findOne(final long id) {
-        return (T) getCurrentSession().get(clazz, id);
-    }
+	@Override
+	public final T findOne(final long id) {
+		return (T) getCurrentSession().get(clazz, id);
+	}
 
-    @Override
-    public final List<T> findAll() {
-        return getCurrentSession().createQuery("from " + clazz.getName()).list();
-    }
+	@Override
+	public final List<T> findAll() {
+		return getCurrentSession().createQuery("from " + clazz.getName()).list();
+	}
 
-    @Override
-    public final void create(final T entity) {
-        Preconditions.checkNotNull(entity);
-        // getCurrentSession().persist(entity);
-        getCurrentSession().saveOrUpdate(entity);
-    }
+	@Override
+	public final void create(final T entity) {
+		// getCurrentSession().persist(entity);
+		getCurrentSession().saveOrUpdate(entity);
+	}
 
-    @Override
-    public final T update(final T entity) {
-        Preconditions.checkNotNull(entity);
-        return (T) getCurrentSession().merge(entity);
-    }
+	@Override
+	public final T update(final T entity) {
+		return (T) getCurrentSession().merge(entity);
+	}
 
-    @Override
-    public final void delete(final T entity) {
-        Preconditions.checkNotNull(entity);
-        getCurrentSession().delete(entity);
-    }
+	@Override
+	public final void delete(final T entity) {
+		getCurrentSession().delete(entity);
+	}
 
-    @Override
-    public final void deleteById(final long entityId) {
-        final T entity = findOne(entityId);
-        Preconditions.checkState(entity != null);
-        delete(entity);
-    }
+	@Override
+	public final void deleteById(final long entityId) {
+		final T entity = findOne(entityId);
+		delete(entity);
+	}
 
-    protected final Session getCurrentSession() {
-        return sessionFactory.getCurrentSession();
-    }
+	protected final Session getCurrentSession() {
+		return sessionFactory.getCurrentSession();
+	}
 
 }
