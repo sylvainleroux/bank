7b9cb52b42f67f7b6d43dc7a84a809628cfa6394
Sylvain LE ROUX <sylvain.leroux@gmail.com>
2018-02-23 21:28:08 +0100

    Improve dump script

--- dump.sh
+++ dump.sh
@@ -1,5 +1,15 @@
-DUMP_DATE=$(date +"%Y%m%d%H%m%s")
+#!/bin/bash 
 
-mysqldump -h192.168.1.17 -usleroux -p bank > ~/Dropbox/Comptes/dump_$DUMP_DATE.sql
+DUMP_DATE=$(date +"%Y-%m-%d_%H%m%s")
+HOST=192.168.1.71
+
+# touch ~/.my.cnf
+# chmod 600 ~/.my.cnf
+# [mysqldump]
+# user=sleroux
+# password=XXXXX
+
+echo "Start dump ..."
+mysqldump --routines  -h$HOST -usleroux bank > ~/Dropbox/Comptes/dump_$DUMP_DATE.sql
 
 ls -lh ~/Dropbox/Comptes/dump_$DUMP_DATE.sql
