dfb02dad80ef3dada5830283b6f3d7fa5d5bc3bc
Sylvain LE ROUX <sylvain.leroux@gmail.com>
2018-02-23 21:13:52 +0100

    Restore phantom path

--- crawler/run-cmb.sh
+++ crawler/run-cmb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# export PATH=$PATH:/usr/local/Cellar/node/4.2.1/lib/node_modules/phantomjs/lib/phantom/bin
-# PATH=$PATH:/usr/local/bin
+ export PATH=$PATH:/usr/local/Cellar/node/4.2.1/lib/node_modules/phantomjs/lib/phantom/bin
+ PATH=$PATH:/usr/local/bin
 
 rm -Rf tmp/*
 phantomjs --cookies-file=tmp/cmb-cookies scripts/cmb.js
